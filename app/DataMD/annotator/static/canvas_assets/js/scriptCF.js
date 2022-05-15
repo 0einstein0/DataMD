53eb51d6ed1a661f9ce4bce9f3d751e3772548b6
0einstein0 <fzulfiqar.bscs18seecs@seecs.edu.pk>
2022-05-15 13:11:43 +0500

    Added keys to labels

--- app/DataMD/annotator/static/canvas_assets/js/scriptCF.js
+++ app/DataMD/annotator/static/canvas_assets/js/scriptCF.js
@@ -19,9 +19,9 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("activeImg").src = images[currentImage];
   }
 
-  function selectColor(colorNum, colors){
+  function selectColor(colorNum, colors) {
     if (colors < 1) colors = 1; // defaults to one color - avoid divide by zero
-    return "hsl(" + (colorNum * (360 / colors) % 360) + ",63%,50%)";
+    return "hsl(" + ((colorNum * (360 / colors)) % 360) + ",63%,50%)";
   }
   ////////////
 
@@ -51,56 +51,20 @@ document.addEventListener("DOMContentLoaded", function () {
   var labelBtn;
   var labelText;
   var btnDiv = document.getElementById("labelBtns");
+  var keyArray = ["A", "S", "D", "F", "G"];
+  var labelKey;
 
   for (var i = 0; i < labelsNo; ++i) {
     labelBtn = document.createElement("button");
-      labelBtn.style.backgroundColor = selectColor(i, labelsNo);
-      labelText = document.createTextNode(possible_labels[i]);
-      labelBtn.appendChild(labelText);
-      btnDiv.appendChild(labelBtn);
+    labelBtn.style.backgroundColor = selectColor(i, labelsNo);
+    labelText = document.createTextNode(possible_labels[i]);
+    labelKey = document.createTextNode("Press: " + keyArray[i]);
+    linebreak = document.createElement("br");
+    labelBtn.appendChild(labelText);
+    labelBtn.appendChild(linebreak);
+    labelBtn.appendChild(labelKey);
+    btnDiv.appendChild(labelBtn);
   }
-
-  /*switch (labelsNo) {
-    case 1:
-      labelBtn = document.createElement("button");
-      labelBtn.style.backgroundColor = "rgba(255, 0, 0, 0.5)";
-      labelText = document.createTextNode(possible_labels[0]);
-      labelBtn.appendChild(labelText);
-      btnDiv.appendChild(labelBtn);
-      break;
-    case 2:
-      labelBtn = document.createElement("button");
-      labelBtn.style.backgroundColor = "rgba(255, 0, 0, 0.5)";
-      labelText = document.createTextNode(possible_labels[0]);
-      labelBtn.appendChild(labelText);
-      btnDiv.appendChild(labelBtn);
-      labelBtn = document.createElement("button");
-      labelBtn.style.backgroundColor = "rgba(0, 0, 255, 0.5)";
-      labelText = document.createTextNode(possible_labels[1]);
-      labelBtn.appendChild(labelText);
-      btnDiv.appendChild(labelBtn);
-      break;
-    case 3:
-      labelBtn = document.createElement("button");
-      labelBtn.style.backgroundColor = "rgba(255, 0, 0, 0.5)";
-      labelText = document.createTextNode(possible_labels[0]);
-      labelBtn.appendChild(labelText);
-      btnDiv.appendChild(labelBtn);
-      labelBtn = document.createElement("button");
-      labelBtn.style.backgroundColor = "rgba(0, 255, 0, 0.5)";
-      labelText = document.createTextNode(possible_labels[1]);
-      labelBtn.appendChild(labelText);
-      btnDiv.appendChild(labelBtn);
-      labelBtn = document.createElement("button");
-      labelBtn.style.backgroundColor = "rgba(0, 0, 255, 0.5)";
-      labelText = document.createTextNode(possible_labels[2]);
-      labelBtn.appendChild(labelText);
-
-      break;
-    // code block
-    default:
-      break;
-  }*/
 
   ////////////////////
 });
